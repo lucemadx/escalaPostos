9f3740767ff4b91dbb0b66e29a83bd407a37d147
LucemadxD <lucemadx@gmail.com>
2018-11-09 23:53:35 -0200

    primeiro EPA

--- newEscala/script.js
+++ newEscala/script.js
@@ -1,8 +1,8 @@
 ﻿var diasAtras = 31;
-function Pessoa (nome, tipoEscala, dataEscala, horario)
+function Pessoa (nome, tipoEscala, dataEscala, postos)
 {
-	this.horario = horario;
 	this.nome = nome;
+	this.postos = postos;
 	this.gerarFolgas = function()
 	{
 		var folgas = {};
@@ -13,55 +13,42 @@ function Pessoa (nome, tipoEscala, dataEscala, horario)
 		{
 			if(tipoEscala == 1)
 			{
-				if(i%10 == (dataEscala-1)%10)
-					folgas[i] = -1;
-				else
 				if(i%10 == (dataEscala)%10)
-					folgas[i] = -2;
+					folgas[i] = "F";
 				else
-				if(i%10 == (dataEscala+5)%10)
-					folgas[i] = -2;
+				if(i%10 == (dataEscala+1)%10)
+					folgas[i] = "DSR";
+				else
+				if(i%10 == (dataEscala+6)%10)
+					folgas[i] = "DSR";
 			}
 			if(tipoEscala == 2)
 			{
-				if(i%10 == (dataEscala-1)%10)
-					folgas[i] = -1;
-				else
 				if(i%10 == (dataEscala)%10)
-					folgas[i] = -2;
+					folgas[i] = "F";
 				else
-				if(i%10 == (dataEscala+4)%10)
-					folgas[i] = -2;
+				if(i%10 == (dataEscala+1)%10)
+					folgas[i] = "DSR";
+				else
+				if(i%10 == (dataEscala+5)%10)
+					folgas[i] = "DSR";
 			}
 			if(tipoEscala == 3)
 			{
-				if(i%7 == (dataEscala-1)%7)
-					folgas[i] = -1;
-				else
 				if(i%7 == (dataEscala)%7)
-					folgas[i] = -2
+					folgas[i] = "F";
+				else
+				if(i%7 == (dataEscala+1)%7)
+					folgas[i] = "DSR";
 			}
 		}
 		return folgas;
 	};
 }
 
-var pessoas = 
-[
-	new Pessoa("Ana", 3, 7, "6h00"),
-	new Pessoa("Milton", 3, 1, "6h00"),
-	new Pessoa("Rosangela", 2, 7, "6h00"),
-	new Pessoa("Sergio", 2, 9, "6h00"),
-	new Pessoa("Gabriel M", 2, 9, "6h00"),
-	new Pessoa("Gabriel C", 2, 8, "6h00"),
-	new Pessoa("Massami", 2, 10, "6h00"),
-	new Pessoa("João Paulo", 1, 6, "5h30"),
-	new Pessoa("Vinicius", 1, 5, "5h30")
-];
+var pessoas = [];
 
 var escala = {};
-var horarios = {};
-
 var postoInicial;
 
 function trocaGabriel(fc, ft)
@@ -70,38 +57,33 @@ function trocaGabriel(fc, ft)
 	escala["Gabriel M"][ft] = 0;
 }
 
-var mesAnterior = 
-{
-"Ana": 		[0, 20, 0, 0, 0, 0, 0, 0, 0],
-"Milton": 	[0, 5, 0, 0, 0, 0, 0, 0, 16],
-"Sergio": 	[0, 0, 4, 6, 4, 3, 3, 0, 0],
-"Gabriel M": 	[0, 0, 3, 3, 4, 4, 4, 3, 0],
-"Rosangela": 	[0, 1, 4, 4, 6, 3, 2, 1, 0],
-"Gabriel C": 	[0, 0, 4, 5, 5, 4, 2, 1, 0],
-"João Paulo": 	[0, 1, 5, 3, 5, 4, 2, 1, 0],
-"Vinicius": 	[0, 0, 4, 3, 5, 5, 3, 1, 0],
-"Massami": 	[0, 1, 3, 4, 3, 6, 3, 1, 0]
-
-};
 
 function obterPessoaMenosEscalada(pessoasEscaladas, posto, dia)
 {
 	var pessoaMenosEscalada = undefined, quantidadeEscalada;
+	
 	for (var k = 0; k < pessoasEscaladas.length; k++)
 	{
-		var quantidadeDias = mesAnterior[pessoasEscaladas[k]][posto];
-		
-		for (var p = 0; p <= dia; p++)
-			if(escala[pessoasEscaladas[k]][p] == posto)
-				quantidadeDias++;
-			
-		if(posto == 3 && horarios[pessoasEscaladas[k]] === "5h30")
-			quantidadeDias /= 3;
-		
-		if(quantidadeDias<=quantidadeEscalada || pessoaMenosEscalada === undefined)
+		for (var permitido in funcPostos[pessoasEscaladas[k]])
 		{
-			pessoaMenosEscalada = pessoasEscaladas[k];
-			quantidadeEscalada = quantidadeDias;
+			if(postos[permitido] == posto)
+			{
+				var quantidadeDias = 0;
+				
+				for (var p = 0; p < dia; p++)
+				{
+					console.log(posto, escala[pessoasEscaladas[k]][p]);
+					if(escala[pessoasEscaladas[k]][p] == posto)
+					{
+						quantidadeDias++;
+					}
+				}
+				if(quantidadeDias<=quantidadeEscalada || pessoaMenosEscalada === undefined)
+				{
+					pessoaMenosEscalada = pessoasEscaladas[k];
+					quantidadeEscalada = quantidadeDias;
+				}
+			}
 		}
 	}
 	return pessoaMenosEscalada;
@@ -125,29 +107,6 @@ function ctrlcv(pessoasEscaladas)
 	}
 }
 
-function definirBilheteria2(dia)
-{
-	var bilheteria = [];
-	for (var pessoa in escala)
-		if(escala[pessoa][dia] == 0 && horarios[pessoa] === "6h00" && pessoa != "Ana"&& pessoa != "Milton" && escala[pessoa][dia-1] != 2)
-			bilheteria.push(pessoa);
-
-	escala[obterPessoaMenosEscalada(bilheteria, 2, dia)][dia] = 2;
-}
-
-function definirRenda(dia)
-{
-	if(escala["Ana"][dia]>=0)
-		escala["Ana"][dia] = 1;
-	else
-		if(escala["Milton"][dia]>=0)
-			escala["Milton"][dia] = 1;
-		else
-		{
-			escala[obterPessoaMenosEscalada(obterPessoasNãoEscaladas(dia), 1, dia)][dia] = 1;
-		}
-}
-
 function obterPessoasNãoEscaladas(dia)
 {
 	var lista = [];
@@ -157,48 +116,33 @@ function obterPessoasNãoEscaladas(dia)
 	return lista;
 }
 
-function definirApoio(dia)
-{
-	if(escala["Milton"][dia] == 0)
-		escala["Milton"][dia] = 8;
-}
 
 function definirPostos(dia)
 {
-	var postoInicial = 3;
-	definirBilheteria2(dia);
-	definirRenda(dia);
-	definirApoio(dia);
 	
 	var pessoasEscaladasGeral = obterPessoasNãoEscaladas(dia);
-	for (var posto = pessoasEscaladasGeral.length+postoInicial-1; posto >= postoInicial; posto--)
-		escala[obterPessoaMenosEscalada(obterPessoasNãoEscaladas(dia), posto, dia)][dia] = posto;
-	
-	substituiRepeticao(dia, pessoasEscaladasGeral);
+	for (var i=0; i<postos.length; i++)
+	{
+		var robert = obterPessoaMenosEscalada(obterPessoasNãoEscaladas(dia), postos[i], dia);
+		if(robert !== undefined)
+			escala[robert][dia] = postos[i];
+	}
+	//substituiRepeticao(dia, pessoasEscaladasGeral);
 }
 	
 
 function substituiRepeticao(dia, pessoasEscaladasGeral)
 {
 	for (var pessoa in escala)
-		if(escala[pessoa][dia] == escala[pessoa][dia-1] && escala[pessoa][dia] !=1 && escala[pessoa][dia] !=8)
+		if(escala[pessoa][dia] == escala[pessoa][dia-1])
 		{
 			for (var i = 0; i<pessoasEscaladasGeral.length; i++)
 				if(pessoasEscaladasGeral[i] == pessoa)
 					pessoasEscaladasGeral.splice(i, 1);
 				
 			var substituto;
-				
-			if(escala[pessoa][dia] !=2)
-				substituto = obterPessoaMenosEscalada(pessoasEscaladasGeral, escala[pessoa][dia], dia);
-			else
-			{
-				var novo = [];
-				for (var j = 0; i<pessoasEscaladasGeral.length; i++)
-					if(horarios[pessoa] == "6h00")
-						novo.push(pessoasEscaladasGeral[i]);
-				substituto = obterPessoaMenosEscalada(novo, 2, dia);
-			}
+
+			substituto = obterPessoaMenosEscalada(pessoasEscaladasGeral, escala[pessoa][dia], dia);
 			
 			var aux = escala[pessoa][dia];
 			escala[pessoa][dia] = escala[substituto][dia];
@@ -216,7 +160,7 @@ function exibeNumFuncionarioDia()
 		var totalPostos = 0;
 		for (var pessoa in escala)
 		{
-			if(escala[pessoa][dia] > 0 && escala[pessoa][dia] < 8)
+			if(escala[pessoa][dia]!="F" && escala[pessoa][dia]!="DSR")
 			{
 				totalPostos++;
 			}
@@ -246,49 +190,7 @@ function exibe()
 		for (var dia in escala[pessoa])
 		{
 			document.write("<td>");
-			switch(escala[pessoa][dia])
-			{
-				case -1:
-					document.write(" F ");
-				break;
-
-				case -2:
-					document.write("DSR");
-				break;
-
-				case 1:
-					document.write("1;2");
-				break;
-
-				case 2:
-					document.write("6;2");
-				break;
-
-				case 3:
-					document.write("2;6");
-				break;
-
-				case 4:
-					document.write("3;5");
-				break;
-
-				case 5:
-					document.write("5;3");
-				break;
-
-				case 6:
-					document.write("7");
-				break;
-
-				case 7:
-					document.write("4");
-				break;
-
-				default:
-					document.write(escala[pessoa][dia]);
-				break;
-
-			}
+			document.write(escala[pessoa][dia]);
 			document.write("</td>");
 		}
 
@@ -299,19 +201,31 @@ function exibe()
 	}
 	exibeNumFuncionarioDia();
 	document.write("</table>");
+	document.write("<a href='?'>Voltar para a página anterior</a>");
 }
-
+var funcPostos = {};
 function init()
 {
+	for(var i = 0; i<funcionarios.length; i++)
+	{
+		funcPostos[funcionarios[i]] = [];
+		for(var j = 0; j<postos.length; j++)
+		{
+			if(document.getElementById(funcionarios[i]+postos[j]).checked)
+			{
+				funcPostos[funcionarios[i]].push(postos[j]);
+			}
+		}
+	}
+	for (var i=0; i<funcionarios.length; i++)
+	{
+		pessoas.push(new Pessoa(funcionarios[i], parseInt(document.getElementById("sel"+funcionarios[i]).value), parseInt(document.getElementById("num"+funcionarios[i]).value), funcPostos[funcionarios[i]]));
+	}
 	for (var i = 0; i<pessoas.length; i++)
 	{
 		escala[pessoas[i].nome] = pessoas[i].gerarFolgas();
-		horarios[pessoas[i].nome] = pessoas[i].horario;
 	}
-	trocaGabriel(6, 8);
-	trocaGabriel(20, 19);	
-	trocaGabriel(27, 28);
-	
+
 	for (var dia in escala[pessoas[0].nome])
 		definirPostos(dia);
 
@@ -323,5 +237,37 @@ function init()
 
 	exibe();
 }
-init();
+//init();
+var funcionarios = [];
+var postos = [];
+function gerarEquipe()
+{
+	funcionarios = document.getElementById("funcionarios").value.match(/^..*$/gm);
+	postos = document.getElementById("postos").value.match(/^..*$/gm);
+	document.getElementById("right").style.display = "initial";
+	document.getElementById("escala").innerHTML = "<tr><th colspan='3'><h2>Definição de Escalas</h2></th></tr><tr><th>Nome do Funcionário</th><th>Tipo de Escala</th><th>Dia da 1ª Folga (F)</th></tr>";
+	for (var i = 0; i<funcionarios.length; i++)
+	{
+		document.getElementById("escala").innerHTML += "<tr><td>"+funcionarios[i]+"</td><th><select id='sel"+funcionarios[i]+"'><option value='1'>4132</option><option value='2'>4231</option><option value='3'>5X2</option></select></th><td><input type='number' id='num"+funcionarios[i]+"'/></td></tr>";
+	}
+	var stringHtml;
+	stringHtml = "<tr><th colspan='"+(postos.length+1)+"'><h2>Permissao de Postos</h2></th></tr>"
+	stringHtml +="<tr><td></td>";
+	for (var j = 0; j<postos.length; j++)
+	{
+		stringHtml+="<th>"+postos[j]+"</th>"
+	}
+	stringHtml +="</tr>";
+	for (i = 0; i<funcionarios.length; i++)
+	{
+		stringHtml += "<tr><td>"+funcionarios[i]+"</td>";
+		for (j = 0; j<postos.length; j++)
+		{
+			stringHtml +="<td><input checked='true' type='checkbox' id='"+funcionarios[i]+postos[j]+"' /></td>"
+		}
+		stringHtml += "</tr>";
+	}
+	document.getElementById("funcPosto").innerHTML = stringHtml;
+}
+
 
